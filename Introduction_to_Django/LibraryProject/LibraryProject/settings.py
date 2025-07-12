5965310457d50fdb1a288b308f784e6947ee9ef9
AlvinKarani <alvinmudembei@gmail.com>
2025-07-12 16:45:46 +0300

    corrections for 2

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf',  # This is the app for managing books
 ]
 
 MIDDLEWARE = [
