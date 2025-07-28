c9f6532692abb06978279f23a6668c6fe525a50c
AlvinKarani <alvinmudembei@gmail.com>
2025-07-28 20:50:17 +0300

    i

--- advanced_features_and_security/LibraryProject/relationship_app/forms.py
+++ advanced_features_and_security/LibraryProject/relationship_app/forms.py
@@ -5,3 +5,12 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author']
+
+class ExampleForm(forms.Form):
+    example_field = forms.CharField(max_length=100, required=True, help_text="Enter an example value.")
+    
+    def clean_example_field(self):
+        data = self.cleaned_data['example_field']
+        if not data.isalnum():
+            raise forms.ValidationError("This field must contain only alphanumeric characters.")
+        return data
