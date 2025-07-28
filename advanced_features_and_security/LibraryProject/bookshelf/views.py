9853132e56aab193c49683d9ef50e09c0174daff
AlvinKarani <alvinmudembei@gmail.com>
2025-07-28 20:55:43 +0300

    q

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
+from .forms import BookForm
+from .models import Book
 
 @permission_required('bookshelf.can_edit', raise_exception=True)
 def edit_book(request, pk):
