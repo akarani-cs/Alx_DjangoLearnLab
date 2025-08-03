024ded60156575e96e987094e0c5e61a7ff51a87
AlvinKarani <alvinmudembei@gmail.com>
2025-08-03 17:20:19 +0300

    api 3

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from .models import Book
-from  .serializers import BookSerializer
+from .serializers import BookSerializer
 from rest_framework import generics 
 
-class Booklist(generics.ListAPIView):
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()  # Queryset to retrieve all Book objects
     serializer_class = BookSerializer  # Serializer to convert Book objects to JSON format          
 
